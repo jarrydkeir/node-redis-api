673412a427e38fa6f15002f4dbfb63c23c889337
Jarryd K <jkeir@cenet.catholic.edu.au>
2020-05-07 16:41:55 +1000

    update readme and change api structure

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-Nodejs
+#Overview
+This is a nodejs application that can consume JSON documents to be stored in a standalone redis instance.  
 
-redis - running a docker
+It has the capability to:
+.Create entry
+.Update entry
+.Read entry 
+.Remove entry
+
+##Required Nodejs Packages
+.redis
+.express
+.bodyparser
+
+#Quick Setup
+1. pull this repo
+2. docker pull redis
+3. docker run -p 6379:6379 redis
+4. node app.js
+
+#API Endpoints
+This section covers the API endpoints and their use.
+
+
+###Coming Soon
--- app.js
+++ app.js
@@ -1,18 +1,62 @@
 var express = require('express');
 var redis = require('redis');
 var bodyParser = require('body-parser');
-var app_redis = redis.createClient('6379','172.17.0.2');
+var app_redis = redis.createClient();
 var app = express();
 var fs = require("fs");
 app.use(bodyParser.json());
 
- app.post('/addKey/:id', function(req, res) {
-    console.log(JSON.stringify(req.body,null,2));
+
+///redis/value grouping
+ app.post('/redis/value/:id', function(req, res) {
     app_redis.set(req.params.id,JSON.stringify(req.body,null,2));
-    //console.log(app_redis.get(req.params.id, redis.print));
-    redis_res = app_redis.get(req.params.id, redis.print);
-    res.send(redis_res);
+    redis_res = app_redis.get(req.params.id, function (error,result) {
+        if (error) {
+            console.log(error);
+            throw error;
+        }
+        res.setHeader('Content-Type','application/json');
+        res.send(result);
+    });
  })
+
+ app.get('/redis/value/:id', function(req,res) {
+    redis_res = app_redis.exists(req.params.id, function(error, result){
+        if (result=='0') {
+            res.status(404).send();
+        }
+        if (result=='1'){
+        redis_res = app_redis.get(req.params.id, function (error,result) {
+            if (error) {
+                console.log(error);
+                res.status(500).send(error);
+            }
+            res.setHeader('Content-Type','application/json');
+            res.send(result);
+        });
+    }})
+    
+ })
+
+ //redis/key grouping
+ app.get('/redis/key/:id', function(req,res) {
+    redis_res = app_redis.exists(req.params.id, function(error, result){
+        if (result=='0') {
+            res.status(404).send();
+        }
+        if (result=='1'){
+        redis_res = app_redis.get(req.params.id, function (error,result) {
+            if (error) {
+                console.log(error);
+                res.status(500).send(error);
+            }
+            res.setHeader('Content-Type','application/json');
+            res.send(result);
+        });
+    }})
+    
+ })
+
  
  var server = app.listen(8081, function () {
     var host = server.address().address
