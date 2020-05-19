8d436a0fcb3cea32296541acf55809f57c7e27ff
Jarryd K <jkeir@cenet.catholic.edu.au>
2020-05-19 20:04:59 +1000

    completed /key/:id apis

--- README.md
+++ README.md
@@ -13,8 +13,9 @@ It has the capability to:
 * redis
 * express
 * bodyparser
+* compression
 
->npm install -g redis express bodyparser
+>npm install -g redis express bodyparser compression
 
 # Quick Setup
 
@@ -38,6 +39,8 @@ Where ever :id is used it requires a unique key for redis
 * /keys
     - GET - Retrieves all keys
     - DELETE - Deletes all keys
+    - PUT - Unsupported HTTP Method
+    - POST - Unsupported HTTP Method
 
 * /keys/:id
     - GET - Retrieve value from key
@@ -47,6 +50,9 @@ Where ever :id is used it requires a unique key for redis
 
 * /keys/:id?rename=:newId
     - PUT - Rename key
+    - GET - Unsupported HTTP Method
+    - POST - Unsupported HTTP Method
+    - DELETE - Unsupported HTTP Method
 
 * /key/:id?TTL=3600
     - POST - Create key with a Time to live of 3600 seconds (1 hour)
@@ -95,20 +101,16 @@ Where ever :id is used it requires a unique key for redis
 * Error checking
 * pretty error messages
 * handle unsupported HTTP methods
+* Investigate express.router (see how params work specifically)
 
 
 ### TODO - API Endpoints
-* Flushall - deletes all keys in database - return 204
-* DEL - Delete key - return 204
-* Exists - check if key exists - 200 present - 404 not present
-* Append - update existing key - return 200
 * SETEX - set key with expiry time
 * EXPIRE - set expiry time in secs from now on pre-existing key (PEXPIRE - milliseconds)
 * EXPIREAT - set expiry time - calc secs from today to provided date - validation required for dates that are less than today (PEXIPREAT - milliseconds)
 * PERSIST - remove TTL on key
 * RENAME - rename key
 * Search Keys - search keys same as exists
-* SORT - may not be required
 * TTL - Check TTL on key (secs) - maybe add formatting date.now + time UTC
 * GETSET - provide new data set for key and return old dataset
 * HGET
--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var redis = require('redis');
 var bodyParser = require('body-parser');
+var compression = require('compression');
 var app_redis = redis.createClient();
 var app = express();
 var fs = require("fs");
 app.use(bodyParser.json());
+app.use(compression());
 
 // /key grouping
-
 app.get('/keys',function(req, res) {
 
     res_redis = app_redis.keys('*', function(redisKeysError, redisKeysResult)
@@ -21,59 +22,111 @@ app.get('/keys',function(req, res) {
 })
 
 app.delete('/keys', function(req,res) {
-    app_redis.flushall;
-    res.status(204).send();
+    app_redis.flushall(function(redisDeleteAllError, redisDeleteAllResult){
+        if (redisDeleteAllError){
+            //Do something not sure what - send back error??
+        }
+        res.status(204).send(redisDeleteAllResult);
+    })
+
 })
 
 app.post('/keys', function(req,res){
-    res.status(405).json({"error":"Method Not Allowed"})
+    res.status(405).json({"error":"Method Not Allowed","detail":"Please provide new key name to create a new key"})
+})
+
+app.put('/keys', function(req,res)
+{
+    res.status(405).json({"error":"Method Not Allowed","detail":"Please provide pre-existing key to update"})
 })
 
 
-
 // /key/:id grouping
- app.post('/keys/:id', function(req, res) {
-     //TODO Compress Json before storing
-     //TODO check content-type reject if not - add content type as const so it can be flexible
-    app_redis.set(req.params.id,JSON.stringify(req.body,null,2));
-    redis_res = app_redis.get(req.params.id, function (error,result) {
-        if (error) {
-            console.log(error);
-            throw error;
+ app.post('/keys/:id', function(req, res) 
+ {
+    if (req.header("Content-Type") == 'application/json') 
+    {   
+    app_redis.get(req.params.id, function (redisGetError,redisGetResult) {
+        if (redisGetResult == 0) {
+            res.status(400).json({"error":"Key Does Not Exists","details":"The provided Key does not exist, if you want to create this key please use HTTP Method POST"})
+
+        } if (redisGetResult == 1) {
+            app_redis.set(req.params.id,JSON.stringify(req.body,null,2)); // add check to ensure that key has been set
+            redis_res = app_redis.get(req.params.id, function (redisGetError,redisGetResult) {
+                if (redisGetError) {
+                    console.log(redisGetError);
+                    throw redisGetError;
+                }
+                res.status(200).json(JSON.parse(redisGetResult));
+            });
         }
-        res.setHeader('Content-Type','application/json');
-        res.send(result);
-    });
+    })
+    } else {
+        res.status(415).json({"error":"Media Type Not Supported","details":"Content-Type not supported. Make sure that provided value is JSON and Content-Type is set to application/json"})
+    }
  })
 
- app.get('/keys/:id', function(req,res) {
-     //TODO - Check content-type
+ app.put('/keys/:id', function(req, res) 
+ {
+    if (req.header("Content-Type") == 'application/json') 
+    {   
+    app_redis.get(req.params.id, function (redisGetError,redisGetResult) {
+        if (redisGetResult) {
+            res.status(400).json({"error":"Key Exists","details":"A key / value pair already exists, if you want to update this key please use HTTP Method PUT"})
 
-    redis_res = app_redis.exists(req.params.id, function(error, result){
-        if (result=='0') {
-            res.status(404).send();
+        } else {
+            app_redis.set(req.params.id,JSON.stringify(req.body,null,2)); // add check to ensure that key has been set
+            redis_res = app_redis.get(req.params.id, function (redisGetError,redisGetResult) {
+                if (redisGetError) {
+                    console.log(redisGetError);
+                    throw redisGetError;
+                }
+                res.status(200).json(JSON.parse(redisGetResult));
+            });
         }
-        if (result=='1'){
-        redis_res = app_redis.get(req.params.id, function (error,result) {
-            if (error) {
-                console.log(error);
-                res.status(400).send(error);
-            } if (result !== null){
-                //TODO Move content-type to const
-            res.setHeader('Content-Type','application/json');
-            res.send(result);
-        }
-        });
-    }})
-    
+    })
+    } else {
+        res.status(415).json({"error":"Media Type Not Supported","details":"Content-Type not supported. Make sure that provided value is JSON and Content-Type is set to application/json"})
+    }
  })
+
+ app.get('/keys/:id', function(req,res) 
+ {
+
+    redis_res = app_redis.exists(req.params.id, function(redisExistError, redisExistResult)
+        {
+            if (result=='0') 
+            {
+                res.status(404).json({"error":"Not found","details":"Key is not found"});
+            }
+            if (result=='1')
+            {
+            redis_res = app_redis.get(req.params.id, function (redisGetError,redisGetResult) {
+                if (redisGetError) {
+                    res.status(500).json(redisGetError);
+                } if (redisGetResult !== null)
+                {
+                res.status(200).json(JSON.parse(redisGetResult));
+                } else
+                {
+                    res.status(204).json();
+                }
+            });
+            }
+        })
+    })
 
  app.delete('/keys/:id', function(req,res) {
     if (req.params.id !==null) {
-        //validate key then delete
-        app_redis.del(req.params.id, function(redisDelKeyError, redisDelKeyResult)
+        redis_res = app_redis.exists(req.params.id, function(redisExistError, redisExistResult)
         {
-            
+            if (result=='0') 
+            {
+                res.status(404).json({"error":"Not found","details":"Key is not found"});
+            }
+            if (result=='1') {
+                app_redis.del(req.params.id, function(redisDelKeyError, redisDelKeyResult) {
+                    res.status(204).json();
         }
         )
     }
